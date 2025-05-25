f062aeee9d968b86e3c8c9ef95e02002c2b97c50
Igor Costa <igor.costa@tecnologia-i.com.br>
2025-05-24 22:30:43 -0300

    Getting objects from the S3

--- index.mjs
+++ index.mjs
@@ -1,12 +1,26 @@
 import { log } from "./log.mjs";
+import { S3 } from "@aws-sdk/client-s3";
+
+const s3Client = new S3({
+    region: process.env.AWS_REGION
+});
+
 export const handler = async (event) => {
-  const response = {
-    statusCode: 200,
-    body: `<html><body>Dados da requisicao: ${ JSON.stringify(event)}</body></html>`,
-    headers: {
-      "Content-Type": "text/html",      
-    },
-  };
-  log("Log de execução após o GitHub Actions", event);
-  return response;
+    const record = event.Records[0];
+    const bucket = record.s3.bucket.name;
+    const key = record.s3.object.key;
+
+    const getObjectResult = await s3Client.getObject({
+        Bucket: bucket,
+        Key: key
+    });
+
+    const mega_bytes = 1024 * 1024;
+
+    if (getObjectResult.ContentLength > 1 * mega_bytes) {
+        log("Arquivo muito grande", event);
+        return response;
+    };
+    log("Objeto tamanho correto", event);
+    return "Objeto tamanho correto";
 };
