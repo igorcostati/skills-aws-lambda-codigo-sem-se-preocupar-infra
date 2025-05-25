6ab974e06f847363a50510a256d439bcddf66351
Igor Costa <igor.costa@tecnologia-i.com.br>
2025-05-24 21:49:22 -0300

    Alter content to html

--- index.mjs
+++ index.mjs
@@ -2,7 +2,10 @@ import { log } from "./log.mjs";
 export const handler = async (event) => {
   const response = {
     statusCode: 200,
-    body: JSON.stringify(event),
+    body: `<html><body>Dados da requisicao: ${ JSON.stringify(event)}</body></html>`,
+    headers: {
+      "Content-Type": "text/html",      
+    },
   };
   log("Log de execução após o GitHub Actions", event);
   return response;
